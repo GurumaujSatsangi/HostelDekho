d8c0e9baf812a2382d090f3b8f27bfcd80a4fd33
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2025-07-24 09:51:11 +0530

    hostel page modified

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ import bodyParser from "body-parser";
 import ejs from "ejs";
 import sql from "mssql";
 import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 const app = express();
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+});
+
+app.get("/view/hostel/:id", async (req, res) => {
+  const { data, error } = await supabase
+    .from("hostels")
+    .select("*")
+    .eq("hostel_id", req.params.id)
+    .single();
+
+  const { floorplan, floorplanerror } = await supabase
+    .from("floor_plans")
+    .select("*")
+    .eq("hostel_id", req.params.id);
+
+  return res.render("hostel.ejs", { hosteldata: data, floors: floorplan });
 });
 
 app.listen(3000, () => {
