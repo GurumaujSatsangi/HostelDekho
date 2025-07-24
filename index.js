df968547da0aa2bfa4bc55bb3c30de5138bfa8ea
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2025-07-24 13:34:24 +0530

    floorplan page added

--- index.js
+++ index.js
@@ -36,6 +36,26 @@ app.get("/view/hostel/:id", async (req, res) => {
   return res.render("hostel.ejs", { hosteldata: data, floorplan });
 });
 
+app.get("/view/hostel/:hostelid/floor-plan/:planid", async (req,res)=> {
+
+  const { data, error } = await supabase
+    .from("hostels")
+    .select("*")
+    .eq("hostel_id", req.params.hostelid)
+    .single();
+
+
+  const { data: floorplan, error: floorplanerror } = await supabase
+    .from("floor_plans")
+    .select("*")
+    .eq("hostel_id", req.params.hostelid)
+    .eq("id",req.params.planid)
+    .single();
+
+    return res.render("floorplan.ejs", {  hosteldata: data, floorplan });
+
+})
+
 app.listen(3000, () => {
   console.log("Running on Port 3000!");
 });
