482b72713750dac0255628961a7fbcbe35f7dfc4
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2025-08-27 22:51:56 +0530

    callback uri updated

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/dashboard",
+      callbackURL: "http://hosteldekho.onrender.com/auth/google/dashboard",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
